8949fd6202ae655e6d3edefea9e0b51e4deec271
AbdulMoizJawed <amoizj16@gmail.com>
2025-10-24 11:27:56 +0500

    removed Unnecassary files

--- app/beats/test-page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function TestBeatsPage() {
-  return (
-    <div className="min-h-screen bg-gray-950 flex items-center justify-center">
-      <h1 className="text-white text-2xl">Test Beats Page</h1>
-    </div>
-  )
-}
