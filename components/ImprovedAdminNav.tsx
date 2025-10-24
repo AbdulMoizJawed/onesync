b400477b72c3666fa519b2d0416905b2f409488e
AbdulMoizJawed <amoizj16@gmail.com>
2025-10-24 10:37:45 +0500

    Responsive

--- components/ImprovedAdminNav.tsx
+++ components/ImprovedAdminNav.tsx
@@ -236,7 +236,7 @@ export default function ImprovedAdminNav({
       </div>
 
       {/* Quick Actions */}
-      <div className="p-4 border-b border-gray-800">
+      <div className="hidden md:block p-4 border-b border-gray-800">
         <h3 className="text-xs font-semibold text-gray-500 uppercase mb-3">Quick Actions</h3>
         <div className="grid grid-cols-2 gap-2">
           <Button
@@ -404,11 +404,11 @@ export default function ImprovedAdminNav({
         {/* Header Bar */}
         <div className="bg-gray-900 border-b border-gray-800 flex-shrink-0">
           <div className="px-4 sm:px-6 lg:px-8">
-            <div className="flex items-center justify-between h-16">
+            <div className="flex items-center md:justify-between justify-end  h-16">
                 <div className='hidden md:block'></div>
-              <div className="flex items-center md:hidden">
+              <div className="items-center hidden md:flex">
                 <Shield className="w-8 h-8 text-blue-400 mr-3" />
-                <h1 className="text-xl font-bold text-white">Admin Dashboard</h1>
+                <h1 className="text-xl font-bold text-white ">Admin Dashboard</h1>
               </div>
               <div className="flex items-center space-x-4">
                 <Badge
